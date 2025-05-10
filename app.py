ca7075cf4e7d20b3839ca23571e03556a90e36af
Suleiman <Suleimanayomide716@gmail.com>
2025-05-10 19:56:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,13 @@ def check_google_title():
 
     options = Options()
     options.binary_location = chrome_bin
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--single-process")
+    #options.add_argument("--single-process")
 
     service = Service(chromedriver_bin)
 
